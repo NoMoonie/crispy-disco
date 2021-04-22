f3a62fa1d4488533466011ca69c8c7a2ffc339e0
Garut <46845957+NoMoonie@users.noreply.github.com>
2021-04-22 02:02:30 +0200

    changed to component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import img from "img/worldinGlassBottle.png";
+import FlipBox from "components/FlipBox";
 
 const HomeStyle = styled.div`
     background-image: url(${img});
@@ -8,56 +9,15 @@ const HomeStyle = styled.div`
     height: 100vh;
     overflow: auto;
     place-content: center;
-`
-
-const FlipBox = styled.div`
-    
-    background-color: transparent;
-    width: 60em;
-    height: 30em;
-    perspective: 1000px;
-`
-const FlipBoxInner = styled.div`
-    position: relative;
-    width: 100%;
-    height: 100%;
-    text-align: center;
-    transition: transform 0.8s;
-    transform-style: preserve-3d;
-`
-
-const BoxFront = styled.div`
-    color: ${props => props.theme.page.textColor};
-    background-color: hsl(282.7722772277228, 100%, 80.19607843137256%, .5); 
-    display: grid; 
-    place-content: center;
-    h1,h2{
-        font-size: 60pt;
-    }
-`
-
-const BoxBack = styled.div`
-    background-color: ${props => props.theme.page.bg};
-    color: ${props => props.theme.page.textColor};
-    transform: rotateY(180deg);
-`
+    overflow: hidden;
+`;
 
 const Home = () => {
     return (
         <HomeStyle>
-           <FlipBox className="flip-box">
-                <FlipBoxInner className="flip-box-inner">
-                    <BoxFront className="flip-box-front">
-                        <h2>Hello!</h2><br/><hr/><br/>
-                        <h1>I'm Emil</h1>
-                    </BoxFront>
-                    <BoxBack className="flip-box-back">
-                        <h2>Back Side</h2>
-                    </BoxBack>
-                </FlipBoxInner>
-           </FlipBox>
+            <FlipBox />
         </HomeStyle>
     );
-}
+};
 
 export default Home;

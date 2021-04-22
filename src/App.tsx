84cc54ec0655085f87a065a5459f21f3ba91c935
Garut <46845957+NoMoonie@users.noreply.github.com>
2021-04-22 13:14:06 +0200

    deleted old cold

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,11 @@ import GlobalStyle from "styled/globalStyle";
 import Theme from "styled/theme.json";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { FaAngleDown, FaItchIo, FaGithub } from "react-icons/fa";
-import firebase from "firebase/app";
 import Home from "pages/Home";
 import About from "pages/About";
 import Navbar from "components/Navbar";
 import Navitem from "components/Navitem";
 import DropDownMenu from "components/DropDownMenu";
-
-/*
-firebase.initializeApp({
-    apiKey: "AIzaSyBDNzhaW36jf_i0DEFPITaKbA9Os_n7rKw",
-    authDomain: "nomoon404.firebaseapp.com",
-    projectId: "nomoon404",
-    storageBucket: "nomoon404.appspot.com",
-    messagingSenderId: "763360472309",
-    appId: "1:763360472309:web:1c2367c7a466edccaaa9bc",
-    measurementId: "G-W9SXEHCS87",
-});
-*/
 
 const App = () => {
     return (

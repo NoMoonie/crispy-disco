ec857e4df03ca0bcaaea8ed0b1c9e66922375861
Garut <46845957+NoMoonie@users.noreply.github.com>
2021-04-23 16:32:15 +0200

    added interface

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,22 @@ import { FC } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const ButtonStyle = styled.div<{ fontsize?: string; width?: string; height?: string }>`
+interface Buttoninterface {
+    link: string;
+    fontsize?: string;
+    width?: string;
+    height?: string;
+    children?: any;
+    bold?: string;
+}
+
+const ButtonStyle = styled.div<{ fontsize?: string; width?: string; height?: string; bold?: string }>`
     background-color: ${(props) => props.theme.button.bg};
     color: ${(props) => props.theme.button.textColor};
     width: ${(props) => (props.width ? props.width : "200px")};
     height: ${(props) => (props.height ? props.height : "100px")};
     font-size: ${(props) => (props.fontsize ? props.fontsize : "20pt")};
-    font-weight: bolder;
+    font-weight: ${(props) => (props.bold ? props.bold : "bolder")};
     border-radius: 10px;
     border: none;
     cursor: pointer;
@@ -25,9 +34,9 @@ const ButtonStyle = styled.div<{ fontsize?: string; width?: string; height?: str
     }
 `;
 
-const Button: FC<any> = ({ link, children }) => {
+const Button: FC<Buttoninterface> = ({ link, fontsize, width, height, children, bold }) => {
     return (
-        <ButtonStyle>
+        <ButtonStyle fontsize={fontsize} width={width} height={height} bold={bold}>
             <Link to={link}>{children}</Link>
         </ButtonStyle>
     );

e83f1e9ce9817175e635e9b32ca7de82c95b14db
Garut <46845957+NoMoonie@users.noreply.github.com>
2021-04-23 16:47:13 +0200

    added styling

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,6 +2,14 @@ import styled from "styled-components";
 
 const AboutStyle = styled.div`
     color: ${(props) => props.theme.main.textColor};
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr;
+    grid-template-rows: 1fr 2fr 1fr;
+    height: 100vh;
+    @media (max-width: 414px) {
+        grid-template-columns: 0.1fr 8fr 0.1fr;
+        grid-template-rows: 0.3fr 2fr 0.1fr;
+    }
 `;
 
 const Home = () => {

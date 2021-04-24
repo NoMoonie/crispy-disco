7b0c742701b2fbcc57282266e179295946556adf
Garut <46845957+NoMoonie@users.noreply.github.com>
2021-04-24 12:48:36 +0200

    added cv temp

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,31 +1,62 @@
 import styled from "styled-components";
+import { MdWork } from "react-icons/md";
+import { FaCertificate } from "react-icons/fa";
 
 const AboutStyle = styled.div`
     color: ${(props) => props.theme.main.textColor};
     display: grid;
-    grid-template-columns: 1fr 5fr 1fr;
-    grid-template-rows: 1fr 5fr 1fr;
-    height: 100vh;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    height: 130vh;
+    gap: 1em;
+    padding: 4em;
+    div {
+        border: ${(props) => props.theme.main.border};
+        border-radius: 10px;
+        h1 {
+            font-weight: normal;
+        }
+        svg {
+            height: 40px;
+            width: 40px;
+        }
+    }
 `;
 
 const Box1 = styled.div`
-    grid-column: 2/2;
-    grid-row: 2/2;
+    grid-row: 1/3;
     background-color: ${(props) => props.theme.main.bg};
-    display: grid;
-    place-items: center;
-    text-align: center;
+    margin-left: 10em;
+`;
+
+const Box2 = styled.div`
+    grid-column: 2/4;
+    background-color: ${(props) => props.theme.main.bg};
+    margin-right: 10em;
+`;
+
+const Box3 = styled.div`
+    grid-column: 2/4;
+    background-color: ${(props) => props.theme.main.bg};
+    margin-right: 10em;
 `;
 
 const Home = () => {
     return (
         <AboutStyle>
             <Box1>
-                <h1>
-                    About
-                    <br /> Comming soon!
-                </h1>
+                <h1>me</h1>
             </Box1>
+            <Box2>
+                <h1>
+                    <MdWork /> Work Experience
+                </h1>
+            </Box2>
+            <Box3>
+                <h1>
+                    <FaCertificate /> Education
+                </h1>
+            </Box3>
         </AboutStyle>
     );
 };

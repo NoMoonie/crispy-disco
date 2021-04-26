0ee4d3fceb32ee11cd6d328bdf5d3e57e8d7d6c5
Garut <46845957+NoMoonie@users.noreply.github.com>
2021-04-27 01:33:53 +0200

    change content

--- src/components/pagesComponets/aboutPage/Skills.tsx
+++ src/components/pagesComponets/aboutPage/Skills.tsx
@@ -3,14 +3,15 @@ import SkillItem from "components/pagesComponets/aboutPage/SkillItem";
 const Skills = () => {
     return (
         <section>
-            <h1>skills</h1>
-            <SkillItem procent="97%" name="React" />
-            <SkillItem procent="97%" name="Javascript" />
-            <SkillItem procent="94%" name="Git/Github" />
-            <SkillItem procent="90%" name="Html" />
+            <h1>Skills</h1>
+            <SkillItem procent="90%" name="React" />
+            <SkillItem procent="80%" name="Javascript" />
+            <SkillItem procent="80%" name="Git/Github" />
+            <SkillItem procent="80%" name="Html" />
             <SkillItem procent="80%" name="Css" />
-            <SkillItem procent="40%" name="Wordpress" />
-            <SkillItem procent="30%" name="php" />
+            <h1>Languages</h1>
+            <SkillItem procent="100%" name="Swedish" />
+            <SkillItem procent="90%" name="English" />
         </section>
     );
 };

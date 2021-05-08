7c91f790ec39ca997620502b536ce5fc6b8644f4
Garut <46845957+NoMoonie@users.noreply.github.com>
2021-05-08 11:37:34 +0200

    removed back color

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,10 +4,6 @@ import { FC } from "react";
 const NavbarStyle = styled.nav`
     z-index: 1;
     height: ${(props) => props.theme.navbar.navSize};
-    /*  position: fixed;
-    top: 0; */
-    background-color: ${(props) => props.theme.main.bg};
-    border-bottom: ${(props) => props.theme.main.scbg} 1px solid;
     width: 100%;
     .navbar-nav {
         max-width: 100%;
